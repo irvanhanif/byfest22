835ae40bb33d9f5a6beb3948bd040eff1347d624
irvanhanif24 <irhan8803@gmail.com>
2022-06-27 22:14:23 +0700

    homepage desktop version

--- front-page.php
+++ front-page.php
@@ -1,6 +1,17 @@
 <?php get_header(); ?>
-<!-- disini yang per page -->
-
-
-<!-- sampai sini -->
+<section id="hp-page">
+    <img id="texture" src="http://brawijayafilmfest.com/wp-content/uploads/2022/06/Layer-4.png" alt="texture">
+    <div id="description">
+        <div id="desc-content">
+            <div id="name">Brawijaya Film Festival</div>
+            <div id="desc-text">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur sit harum, numquam error necessitatibus quae commodi corporis magnam, amet incidunt iusto maiores aut quam exercitationem, architecto rem quos! Dolorum, provident.
+            </div>
+            <a href=""><button id="submit-fp">Submit Now!</button></a>
+        </div>
+    </div>
+    <div class="big-logo">
+        <img src="http://brawijayafilmfest.com/wp-content/uploads/2022/06/logo-bg.png" alt="logo-byfest">
+    </div>
+</section>
 <?php get_footer(); ?>
