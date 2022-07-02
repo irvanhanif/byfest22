ed3413c4e55e90a922cbcda08306dcf4c4892ee7
irvanhanif24 <irhan8803@gmail.com>
2022-07-02 23:17:32 +0700

    update home

--- header.php
+++ header.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php the_title(); ?></title>
+    <link rel="icon" href="http://brawijayafilmfest.com/wp-content/uploads/2022/06/logo-bg.png">
 </head>
 <body>
 <!-- disini kode header -->
