07ea4aea9ce61398761ba96541793edd43d41880
irvanhanif24 <irhan8803@gmail.com>
2022-09-07 22:50:35 +0700

    fix something

--- front-page.php
+++ front-page.php
@@ -2,37 +2,30 @@
 <section id="hp-page">
     <!-- <div class="space"></div> -->
     <img id="texture" src="http://brawijayafilmfest.com/wp-content/uploads/2022/06/Layer-4.png" alt="texture">
-    <div class="big-logo-1">
-        <img src="http://brawijayafilmfest.com/wp-content/uploads/2022/07/1.png" alt="logo-byfest">
-    </div>    
-    <div id="description">
-        <div id="desc-content">
-            <div id="name">Brawijaya Film Festival</div>
-            <div id="desc-text">
-            Brawijaya Film Festival atau biasa dikenal dengan sebutan “BYFEST” adalah rangkaian acara festival film yang diselenggarakan oleh UKM Nol Derajat Film UB. Rangkaian acara BYFEST mencakup pemutaran film panjang lawas, program kompetisi film pendek, forum komunitas dan webinar untuk mengapresiasi film tanah air dari segala era.
+    <div id="top">
+        <div class="big-logo-1">
+            <img src="http://brawijayafilmfest.com/wp-content/uploads/2022/07/1.png" alt="logo-byfest">
+        </div>    
+        <div id="description">
+            <div id="desc-content">
+                <div id="name">Brawijaya Film Festival</div>
+                <div id="desc-text">
+                Brawijaya Film Festival atau biasa dikenal dengan sebutan “BYFEST” adalah rangkaian acara festival film yang diselenggarakan oleh UKM Nol Derajat Film UB. Rangkaian acara BYFEST mencakup pemutaran film panjang lawas, program kompetisi film pendek, forum komunitas dan webinar untuk mengapresiasi film tanah air dari segala era.
+                </div>
+                <!--<a href="http://brawijayafilmfest.com/ketentuan-submisi/"><button id="submit-fp">Daftar Sekarang!</button></a>-->
             </div>
-            <!--<a href="http://brawijayafilmfest.com/ketentuan-submisi/"><button id="submit-fp">Daftar Sekarang!</button></a>-->
+        </div>
+        <div class="big-logo-2">
+            <img src="http://brawijayafilmfest.com/wp-content/uploads/2022/07/1.png" alt="logo-byfest">
         </div>
     </div>
-    <div class="big-logo-2">
-        <img src="http://brawijayafilmfest.com/wp-content/uploads/2022/07/1.png" alt="logo-byfest">
-    </div>
-    </section>
-
-    <section id="meet"> 
-    <div id="mot">
-    <img id="texture" src="http://brawijayafilmfest.com/wp-content/uploads/2022/06/Layer-4.png" alt="texture">
-            <div id="title1">Meet Our Artist</div>
-            <div id="title2">Bagas Mahendra</div>
-            <div id="desc-text1">
-            Bagas Mahendra merupakan salah satu seniman yang berasal dari Pasuruan. Beliau sedang menempuh studi di Universitas Brawijaya jurusan seni rupa murni. Selain melukis, beliau sedang aktif dalam komunitas berbasis kesenirupaan serta menggarap proyek - proyek dari commision. Di beberapa kesempatan, beliau pernah menjadi pemateri dalam kegiatan workshop drawing. 
-            </div>
-            <div id="desc-text2">
-            Pameran - pameran yang pernah diikuti beliau seperti ALKMAART DRAWING (2018), Tunak Tunuk Taneg (2018), Rumahku (2018), Ruang Bebas I (2019), Dies Natalis FIB UB (2019), Ruang Bebas II (2020), Pilu Membiru (2020), Kaleidoskop (2021), Arte Music (2021), Sopankah begitu (2021), Self Matter Exhibition (2021), Epilogue (2022), Ode of java (2022), dan Ngeker (2022).
-            </div>
-            <div id="desc-text3">
-            Salah satu karya beliau pada Brawijaya Film Festival kali ini adalah logo tema besar dari Brawijaya Film Festival tahun ini, Dramaturgi, yang digambarkan seperti dibawah ini.
-            </div>
+    <div id="moa">
+        <div id="title-moa">Meet Our Artist</div>
+        <img id="img-moa" src="http://brawijayafilmfest.com/wp-content/uploads/2022/09/mas-bagas-scaled.jpg" alt="our artist">
+        <div id="title-moa">Bagas Mahendra</div>
+        <div id="desc-text-moa">
+            Bagas Mahendra merupakan salah satu seniman yang berasal dari Pasuruan. Beliau sedang menempuh studi di Universitas Brawijaya jurusan seni rupa murni. Selain melukis, beliau sedang aktif dalam komunitas berbasis kesenirupaan serta menggarap proyek - proyek dari commision. Di beberapa kesempatan, beliau pernah menjadi pemateri dalam kegiatan workshop drawing. <br><br>Pameran - pameran yang pernah diikuti beliau seperti ALKMAART DRAWING (2018), Tunak Tunuk Taneg (2018), Rumahku (2018), Ruang Bebas I (2019), Dies Natalis FIB UB (2019), Ruang Bebas II (2020), Pilu Membiru (2020), Kaleidoskop (2021), Arte Music (2021), Sopankah begitu (2021), Self Matter Exhibition (2021), Epilogue (2022), Ode of java (2022), dan Ngeker (2022). <br><br>Salah satu karya beliau pada Brawijaya Film Festival kali ini adalah logo tema besar dari Brawijaya Film Festival tahun ini, Dramaturgi, yang digambarkan seperti dibawah ini.
+        </div>
     </div>
 </section>
 <?php get_footer(); ?>
