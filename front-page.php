273d696f43d240888194bb8519e8b62fca0c423e
irvanhanif24 <irhan8803@gmail.com>
2022-09-08 21:00:14 +0700

    update homepage

--- front-page.php
+++ front-page.php
@@ -27,5 +27,76 @@
             Bagas Mahendra merupakan salah satu seniman yang berasal dari Pasuruan. Beliau sedang menempuh studi di Universitas Brawijaya jurusan seni rupa murni. Selain melukis, beliau sedang aktif dalam komunitas berbasis kesenirupaan serta menggarap proyek - proyek dari commision. Di beberapa kesempatan, beliau pernah menjadi pemateri dalam kegiatan workshop drawing. <br><br>Pameran - pameran yang pernah diikuti beliau seperti ALKMAART DRAWING (2018), Tunak Tunuk Taneg (2018), Rumahku (2018), Ruang Bebas I (2019), Dies Natalis FIB UB (2019), Ruang Bebas II (2020), Pilu Membiru (2020), Kaleidoskop (2021), Arte Music (2021), Sopankah begitu (2021), Self Matter Exhibition (2021), Epilogue (2022), Ode of java (2022), dan Ngeker (2022). <br><br>Salah satu karya beliau pada Brawijaya Film Festival kali ini adalah logo tema besar dari Brawijaya Film Festival tahun ini, Dramaturgi, yang digambarkan seperti dibawah ini.
         </div>
     </div>
+    <div class="lukisan-1">
+        <img src="http://brawijayafilmfest.com/wp-content/uploads/2022/09/kaos-mas-bagas.jpg" alt="lukisan-1">
+    </div>
+    <div class="lukisan-2">
+        <img src="http://brawijayafilmfest.com/wp-content/uploads/2022/09/kaos-mas-bagas-2.jpg" alt="lukisan-2">
+    </div>
+    <div id="dramaturgi">
+        <div id="title-dramaturgi">
+            <img src="http://brawijayafilmfest.com/wp-content/uploads/2022/09/Layer-4-copy.png" alt="title-dramaturgi">
+        </div>
+        <div id="elemen">
+            <ul>
+                <li>
+                    <div class="left-element">
+                        <div class="left-d">
+                            <img src="http://brawijayafilmfest.com/wp-content/uploads/2022/09/kucing-2.png" alt="kucing">
+                        </div>
+                        <div class="rigth-d">
+                            <div class="title-desc-d">kucing</div>
+                            <div class="desc-content-d">
+                                menggambarkan kemampuan manusia untuk memiliki kepribadian ganda, layaknya kucing yang bisa menjadi makhluk yang paling disukai dan juga paling dibenci.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="right-element">
+                        <div class="left-d">
+                            <img src="http://brawijayafilmfest.com/wp-content/uploads/2022/09/bunga.png" alt="bunga layu">
+                        </div>
+                        <div class="rigth-d">
+                            <div class="title-desc-d">bunga layu</div>
+                            <div class="desc-content-d">
+                                merefleksikan ironi karena banyak yang menganggap bunga adalah simbol kecantikan. Namun, bunga tidak dipandang jika layu.
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    <div class="middle-element">
+                        <img src="http://brawijayafilmfest.com/wp-content/uploads/2022/07/kucing-byfest-png-mawar-ungu.png" alt="kucing">
+                    </div>
+                </li>
+                <li>
+                    <div class="left-element">
+                        <div class="left-d">
+                            <img src="http://brawijayafilmfest.com/wp-content/uploads/2022/09/topeng-kucing.png" alt="topeng malang">
+                        </div>
+                        <div class="rigth-d">
+                            <div class="title-desc-d">topeng malang</div>
+                            <div class="desc-content-d">
+                                topeng Malangan merupakan kesenian khas yang tumbuh dan hidup di kehidupan masyarakat Malang. topeng adalah simbol bagi manusia yang memiliki peran di panggung kehidupan.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="right-element">
+                        <div class="left-d">
+                            <img src="http://brawijayafilmfest.com/wp-content/uploads/2022/09/roll-pilem-2.png" alt="roll film">
+                        </div>
+                        <div class="rigth-d">
+                            <div class="title-desc-d">roll film</div>
+                            <div class="desc-content-d">
+                                melambangkan sejarah perfilman Indonesia yang panjang dan penuh dengan kisah.
+                            </div>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </div> 
+    </div>
+    <div class="lukisan-3">
+        <img src="http://brawijayafilmfest.com/wp-content/uploads/2022/09/merch-mas-bagas-scaled.jpeg" alt="lukisan-3">
+    </div>
 </section>
 <?php get_footer(); ?>
